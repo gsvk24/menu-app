9bc2e43e9e641600685dfb50a8d65ba209b3f0ca
gsvk24 <gusevkirilll123@gmail.com>
2025-07-16 15:15:59 +0300

    feat: new branch with dependencies adn mock-menu  installation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import MenuCard from "@/components/MenuCard";
 import MenuForm from "@/components/MenuForm";
 import SearchBar from "@/components/SearchBar";
 import Modal from "@/components/Modal";
+import { starterItems } from "../../lib/constants";
 
 const containerStyle = {
   width: "100%",
@@ -20,30 +21,6 @@ export default function Page() {
   const [menuItems, setMenuItems] = useState<any[]>([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const starterItems = [
-    {
-      id: 1,
-      name: "Classic Burger",
-      description: "Beef cutlet, cheese, salad, sauce",
-      price: 350,
-      image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd",
-    },
-    {
-      id: 2,
-      name: "Caesar salad",
-      description: "Chicken, lettuce, croutons, sauce",
-      price: 280,
-      image: "https://images.unsplash.com/photo-1546793665-c74683f339c1",
-    },
-    {
-      id: 3,
-      name: "Tiramisu",
-      description: "Classic Italian dessert",
-      price: 220,
-      image: "https://images.unsplash.com/photo-1563805042-7684c019e1cb",
-    },
-  ];
 
   useEffect(() => {
     if (menuItems.length === 0) {
